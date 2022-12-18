8031604c19d11ce0d13ae26b133a2f3076a7ef9e
risqi <risqiikhsani16@gmail.com>
2022-12-18 20:34:38 +0700

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,31 +9,38 @@ import Typography from "@mui/material/Typography";
 import { Stack } from "@mui/material";
 import { Roll } from "react-reveal";
 
+import cat1 from "../images/cat1.jpeg"
+import cat2 from "../images/cat2.jpeg"
+import cat3 from "../images/cat3.jpeg"
+import cat4 from "../images/cat4.jpeg"
+import cat5 from "../images/cat5.jpeg"
+import cat6 from "../images/cat6.jpeg"
+
 const data = [
   {
     name: "Risqi Ikhsani",
     nim: "195410259",
-    picture: "cat6.jpeg",
+    picture: cat6,
   },
   {
     name: "Mawardi Janitra Hadi",
     nim: "215410095",
-    picture: "cat2.jpeg",
+    picture: cat2,
   },
   {
     name: "Akira Wahyu Saputra",
     nim: "215410111",
-    picture: "cat4.jpeg",
+    picture: cat3,
   },
   {
     name: "Steven Natanael Oey",
     nim: "215410127",
-    picture: "cat1.jpeg",
+    picture: cat4,
   },
   {
     name: "Zaki Nedhiansyah",
     nim: "215410141",
-    picture: "cat3.jpeg",
+    picture: cat5,
   },
 ];
 
@@ -58,7 +65,7 @@ export default function About() {
             <>
               <ListItem alignItems="flex-start" key={a.name}>
                 <ListItemAvatar>
-                  <Avatar alt={a.name} src={process.env.PUBLIC_URL + a.picture} sx={{ width: 80, height: 80 }}/>
+                  <Avatar alt={a.name} src={a.picture} sx={{ width: 80, height: 80 }}/>
                 </ListItemAvatar>
                 <ListItemText
                     sx={{ml:'20px',mt:'20px'}}
